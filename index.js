5dcb453dbd137c0d41636b2ed04fb773415e8f63
martinh <martinharrysson@live.se>
2025-01-14 11:56:06 +0100

    Testing pipeline changing replicas from 2 pods to 4 pods

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.send(
-    "Hej David inifrån applikationen som körs i en pod i ett kubernetes-kluster"
+    "Hej David inifrån applikationen som körs i en pod i ett kubernetes-kluster, testar pipelinen genom att ändra till 4 replicas istället för 2"
   );
 });
 
